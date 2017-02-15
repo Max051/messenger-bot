faa10381a967a11848d655b0fabec42eaf469775
Kacper <ciecinski.kacper@gmail.com>
2017-02-15 20:56:42 +0100

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :facebook_id, presence:true, uniqueness: true
-  ActiveRecord::Base.establish_connection('postgres://wrdioimyajmmae:92a7805c4193b16c0a77a4a2cc0ce773d92b5798acc2876802a90fa5062de525@ec2-176-34-111-152.eu-west-1.compute.amazonaws.com:5432/d2t0scao5iojck')
+  ActiveRecord::Base.establish_connection(ENV["DATABASE_HEROKU_URL"])
   def self.find_facebook_user(facebook_id)
     self.where(["facebook_id = ?",facebook_id])
   end

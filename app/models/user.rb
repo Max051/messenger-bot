911ffd011c9bf1bdc96a52f0adfed78ab9f6ec36
Kacper <ciecinski.kacper@gmail.com>
2017-04-14 13:10:22 +0200

    no establish

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :facebook_id, presence:true, uniqueness: true
- # ActiveRecord::Base.establish_connection(ENV["DATABASE_HEROKU_URL"])
+  ActiveRecord::Base.establish_connection(ENV["DATABASE_HEROKU_URL"])
   def self.find_facebook_user(facebook_id)
     self.where(["facebook_id = ?",facebook_id])
   end

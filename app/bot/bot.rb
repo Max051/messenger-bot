7d5802597b3fe58e5cc1c7c043afffa27e1c64a0
Kacper <ciecinski.kacper@gmail.com>
2017-05-17 09:28:55 +0200

    a

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -141,16 +141,16 @@ end
 
   if message.text == 'unsubscribe'
     @user = User.where("facebook_id = ? ",message.sender[:id])
-  #  if !@user.empty?
-     @user.destroy(@user.id)
+  if !@user.empty?
+     User.destroy(@user.id)
      message.reply(
        text:  "I won't send you more messages",
      )
-  #  else
-  #    message.reply(
-  #      text: "You already subscribed or something went wrong",
-  #    )
-  #  end
+    else
+      message.reply(
+        text: "You already subscribed or something went wrong",
+      )
+    end
 end
 
 if message.text == 'help'
